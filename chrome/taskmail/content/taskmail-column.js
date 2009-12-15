eac18fa787fcb0b6d299a9cd46dd6159037c1e2d
gorsini <gorsini@2426cc87-f68e-d5a8-4e0a-d6656307f1a4>
2009-12-15 10:16:24 +0000

    First release

---
+++ chrome/taskmail/content/taskmail-column.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+
+var columnHandler = {
+  isString:            function() {return true;},
+
+  getCellText:         function(row, col) {
+    var taskID = -1;
+    var taskSelectedItem = document.getElementById("taskList").selectedItem; 
+    if (taskSelectedItem != null) {
+      taskID = taskSelectedItem.getAttribute("pk");
+    }
+    var mailKey = gDBView.getKeyAt(row);
+    return getMailTextLink(taskID, mailKey);
+  },
+
+  getCellProperties:   function(row, col, props){},
+
+  getRowProperties:    function(row, props){},
+
+  getImageSrc:         function(row, col) {
+      /*
+      var key = gDBView.getKeyAt(row);
+      var hdr = gDBView.db.GetMsgHdrForKey(key);
+      var messageID= hdr.messageId;
+     	if(hasTask(messageID)==true){
+        return "chrome://xnote/skin/xnote_context.png";
+	    }else {
+		    return null;
+	    }
+	    */
+	    return null;
+  },
+
+  getSortLongForRow:   function(hdr) {return 0;},
+
+  getSortStringForRow: function(hdr) {     
+    var key = gDBView.getKeyAt(row);
+    var hdr = gDBView.db.GetMsgHdrForKey(key);
+    var messageID = hdr.messageId;
+	  return hasTask(messageID);
+  }
+}
+
+window.addEventListener("load", doOnceLoaded, false);
+
+function doOnceLoaded() {
+  var ObserverService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+  ObserverService.addObserver(CreateDbObserver, "MsgCreateDBView", false);
+}
+
+var CreateDbObserver = {
+  // Components.interfaces.nsIObserver
+  observe: function(aMsgFolder, aTopic, aData)
+  {  
+     addCustomColumnHandler();
+  }
+}
+
+function addCustomColumnHandler() {
+   gDBView.addColumnHandler("colTask", columnHandler);
+}
