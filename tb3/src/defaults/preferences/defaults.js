48d2116fd24d0d91de7d1ea29fa4698e7f442eba
gorsini <gorsini@2426cc87-f68e-d5a8-4e0a-d6656307f1a4>
2010-06-22 06:38:39 +0000

    Englishisation

--- tb3/src/defaults/preferences/defaults.js
+++ tb3/src/defaults/preferences/defaults.js
@@ -1,5 +1,5 @@
 pref("taskmail.report.to", "to@domain.fr");
 pref("taskmail.report.cc", "cc@domain.fr");
 pref("taskmail.report.subject", "subject");
-pref("taskmail.report.body", "Rapport hebdo\n#FOLDER#\n<ul>\n    <li>#FOLDER_NAME#</li>\n    <ul>\n       #TASK#<li>#TASK_TITLE# <b>#TASK_STATE#</b> <i>#TASK_DESC#</i></li>#TASK#\n       #SUB_FOLDERS#\n    </ul>\n</ul>\n#FOLDER#\nMerci de prendre en compte.");
+pref("taskmail.report.body", "Report\n#FOLDER#\n<ul>\n    <li>#FOLDER_NAME#</li>\n    <ul>\n       #TASK#<li>#TASK_TITLE# <b>#TASK_STATE#</b> <i>#TASK_DESC#</i></li>#TASK#\n       #SUB_FOLDERS#\n    </ul>\n</ul>\n#FOLDER#\nBest regards.");
 pref("taskmail.states", "chrome://taskmail/locale/taskmail-overlay.properties");
